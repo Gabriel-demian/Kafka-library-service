e90abc96f168cb73e25eea986e93f2d4a3426f45
Liliherax <57782295+Gabriel-demian@users.noreply.github.com>
2024-02-20 17:28:41 -0300

    created PUT endpoint

--- library-events-producer/src/main/java/com/kafka/libraryeventsproducer/domain/LibraryEvent.java
+++ library-events-producer/src/main/java/com/kafka/libraryeventsproducer/domain/LibraryEvent.java
@@ -1,7 +1,6 @@
 package com.kafka.libraryeventsproducer.domain;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record LibraryEvent(
